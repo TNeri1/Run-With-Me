7d72d13bb144ebd8d2cab9ad319c19cdf7dc4087
Michael McFarland <michaelmcfarland1998@gmail.com>
2021-10-27 03:11:20 -0600

    minor activity changes and format

--- app/src/main/java/com/example/runwithme/MealActivity.kt
+++ app/src/main/java/com/example/runwithme/MealActivity.kt
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
 
 class MealActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meal)
+
+        val feedButton = findViewById<ImageButton>(R.id.feed)
+
+        feedButton.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Your Pet is already full!", Toast.LENGTH_LONG).show()
+            })
 
 
 
