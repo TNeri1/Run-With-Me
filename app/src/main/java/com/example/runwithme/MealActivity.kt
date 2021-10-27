e86ac6fe3505496d5810946179708eb0a7a7a5c9
Michael McFarland <michaelmcfarland1998@gmail.com>
2021-10-27 01:16:56 -0600

    Added new activities & working MainActivity buttons

---
+++ app/src/main/java/com/example/runwithme/MealActivity.kt
@@ -0,0 +1,17 @@
+package com.example.runwithme
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+
+class MealActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+
+
+
+    }
+}
